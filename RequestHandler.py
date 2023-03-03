38fcfd90eac8b06b1a3d4bc7b57b1ceb1b2431e9
tushyavemuri <tushya09@gmail.com>
2023-03-03 01:28:44 -0500

    changes

--- RequestHandler.py
+++ RequestHandler.py
@@ -5,5 +5,5 @@ def requestHandler(inRequest):
     print("input request : "+inRequest)
     SU.sysUtils(inRequest)
 
-inRequest = "uptime"
-requestHandler(inRequest)
+#inRequest = "uptime"
+#requestHandler(inRequest)
