3ac2219726f6beffc1886334cd77ef278d63ab41
tushyavemuri <tushya09@gmail.com>
2023-03-22 21:13:34 -0400

    changes

--- systemutils.py
+++ systemutils.py
@@ -1,5 +1,5 @@
 import time
-import psutil
+#import psutil
 import os
 import sys
 import logging
@@ -9,37 +9,43 @@ def sysUtils(inCommand):
     logging.info(f"Started processing input request : {inCommand}")
     if(inCommand == "date"):
         logging.info(f"started")
-        now = datetime.datetime.now()
-        return f"Current date and time: {now.strftime('%Y-%m-%d %H:%M:%S')}"
+        #now = datetime.datetime.now()
+        #return f"Current date and time: {now.strftime('%Y-%m-%d %H:%M:%S')}"
+        return "Date"
     elif(inCommand ==  "uptime"):
         logging.info(f"started")
-        uptime = datetime.datetime.now() - datetime.datetime.fromtimestamp(psutil.boot_time())
-        return f"Server uptime: {str(uptime)}"
+        #uptime = datetime.datetime.now() - datetime.datetime.fromtimestamp(psutil.boot_time())
+        #return f"Server uptime: {str(uptime)}"
+        return "Uptime"
     elif(inCommand ==  "memory"):
         logging.info(f"started")
-        memory = psutil.virtual_memory()
-        return f"Memory usage: {memory.percent}%"
+        #memory = psutil.virtual_memory()
+        #return f"Memory usage: {memory.percent}%"
+        return "memory"
     elif(inCommand ==  "network"):
         logging.info(f"started")
-        netstat = psutil.net_connections()
-        netstat_str = "Netstat:\n"
-        for conn in netstat:
-            netstat_str += f"{conn}\n"
-        return netstat_str
+        #netstat = psutil.net_connections()
+        #netstat_str = "Netstat:\n"
+        #for conn in netstat:
+        #    netstat_str += f"{conn}\n"
+        #return netstat_str
+        return "network"
     elif(inCommand ==  "users"):
         logging.info(f"started")
-        users = psutil.users()
-        users_str = "Current users:\n"
-        for user in users:
-            users_str += f"{user.name}\n"
-        return users_str
+        #users = psutil.users()
+        #users_str = "Current users:\n"
+        #for user in users:
+        #    users_str += f"{user.name}\n"
+        #return users_str
+        return "users"
     elif(inCommand ==  "processes"):
         logging.info(f"started")
-        processes = psutil.process_iter()
-        processes_str = "Running processes:\n"
-        for proc in processes:
-            processes_str += f"{proc.name()}\n"
-        return processes_str
+        #processes = psutil.process_iter()
+        #processes_str = "Running processes:\n"
+        #for proc in processes:
+        #    processes_str += f"{proc.name()}\n"
+        #return processes_str
+        return "processes"
     else:
         logging.info(f"started")
         return "invalid request"
